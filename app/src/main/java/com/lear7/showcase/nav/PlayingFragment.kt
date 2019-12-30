f4168da6ad3da412711d7d00f835ab9328067dc9
lear7 <lear7@qq.com>
2019-12-30 16:38:29 +0800

    fixed bugs

--- app/src/main/java/com/lear7/showcase/nav/PlayingFragment.kt
+++ app/src/main/java/com/lear7/showcase/nav/PlayingFragment.kt
@@ -16,16 +16,16 @@ import kotlinx.android.synthetic.main.fragment_reply.*
 @description
 @date 2019/12/30 14:39
  */
-class PlayingFragment:  Fragment() {
+class PlayingFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_playing, container, false)
 
-        btn_go_home.setOnClickListener({
+        view.findViewById<Button>(R.id.btn_go_home).setOnClickListener({
             val action = PlayingFragmentDirections.actionPlayingFragmentToHomeFragment();
             view.findNavController().navigate(action)
         })
