b8238620a09529c0c13600d260460c377701cfc3
lear7 <lear7@qq.com>
2020-01-16 11:13:55 +0800

    update versions management method

--- app/src/main/java/com/lear7/showcase/nav/demo/LoginFragment.kt
+++ app/src/main/java/com/lear7/showcase/nav/demo/LoginFragment.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.lear7.showcase.R
+import kotlinx.android.synthetic.main.fragment_nav_login.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -23,25 +24,20 @@ import org.jetbrains.anko.support.v4.toast
  */
 class LoginFragment : Fragment() {
 
-    private lateinit var mAccountEd: EditText
-    private lateinit var mLoginBtn: Button
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_nav_login, container, false)
-        mAccountEd = view.findViewById(R.id.et_account)
-        mLoginBtn = view.findViewById(R.id.btn_sign_in)
-        mLoginBtn.setOnClickListener({ login() })
+        btn_sign_in.setOnClickListener({ login() })
 
         return view
     }
 
     fun login() {
         GlobalScope.launch {
-            val account = mAccountEd.text.toString()
+            val account = et_account.text.toString()
             if (TextUtils.isEmpty(account)) {
                 runOnUiThread {
                     toast("请输入账号和密码")
@@ -58,7 +54,7 @@ class LoginFragment : Fragment() {
             toast("登录成功")
         }
 
-        val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment(mAccountEd.text.toString())
+        val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment(et_account.text.toString())
         findNavController().navigate(action)
     }
 }
